5c0254822fe9a991f85ab4d1b3c12dd75241c055
Tom Haile <bradford.haile@gmail.com>
2023-04-05 12:07:36 -0500

    change redirects to to existing pages for testing

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,6 +23,13 @@ const getDocFileNames = () => {
     console.error('Unable to scan directory: ' + error);
   }
 };
+const getRedirects = () => {
+  return JSON.parse(
+    fs
+      .readFileSync(path.join(__dirname, './src/data/redirects.json'))
+      .toString(),
+  );
+};
 /**
  *
  * @param {string} filename
@@ -422,26 +429,8 @@ const config = {
     [
       '@docusaurus/plugin-client-redirects',
       {
-        redirects: [
-          // /docs/oldDoc -> /docs/newDoc
-          {
-            to: '/docs/newDoc',
-            from: '/docs/oldDoc',
-          },
-          // Redirect from multiple old paths to the new path
-          {
-            to: '/docs/newDoc2',
-            from: ['/docs/oldDocFrom2019', '/docs/legacyDocFrom2016'],
-          },
-        ],
+        redirects: getRedirects(),
         createRedirects(existingPath) {
-          if (existingPath.includes('/community')) {
-            // Redirect from /docs/team/X to /community/X and /docs/support/X to /community/X
-            return [
-              existingPath.replace('/community', '/docs/team'),
-              existingPath.replace('/community', '/docs/support'),
-            ];
-          }
           return undefined; // Return a falsy value: no redirect created
         },
       },
