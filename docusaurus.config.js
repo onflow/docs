66865972f6c82c9a0ebe20e2c2d3404c8eb93f0e
Alex Ni <12097569+nialexsan@users.noreply.github.com>
2023-04-28 14:34:36 -0400

    fixed url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -156,7 +156,9 @@ const config = {
   favicon: 'favicon.ico',
 
   // Set the production url of your site here
-  url: process.env.VERCEL_URL || 'https://onflow.github.io',
+  url: process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : 'https://onflow.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: process.env.BASE_URL || '/docs/',
