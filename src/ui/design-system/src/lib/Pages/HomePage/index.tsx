36d77f7dc69a8f229d6b4b904b3c00681e61d51d
Greg Santos <greg.santos@dapperlabs.com>
2023-09-17 12:45:28 -0700

    feat: remove flips section from home

--- src/ui/design-system/src/lib/Pages/HomePage/index.tsx
+++ src/ui/design-system/src/lib/Pages/HomePage/index.tsx
@@ -1,39 +1,39 @@
-import React from 'react';
+import React from "react"
 import {
   Flips,
   LandingHeaderHome,
   HomepageStartList,
   UpcomingEvents,
-} from '../../Components';
+} from "../../Components"
 import {
   ContentNavigationList,
   type ContentNavigationListProps,
-} from '../../Components/ContentNavigationList';
-import { type FlipsProps } from '../../Components/Flips';
-import { type SocialLinksSignupProps } from '../../Components/SocialLinksSignup';
-import { type TutorialCardProps } from '../../Components/TutorialCard';
-import { type UpcomingEventsProps } from '../../Components/UpcomingEvents';
-import PageBackground from '../shared/PageBackground';
-import PageSection from '../shared/PageSection';
-import PageSections from '../shared/PageSections';
-import { type HomepageStartItemProps } from '../../Components/HomepageStartItem';
+} from "../../Components/ContentNavigationList"
+import {type FlipsProps} from "../../Components/Flips"
+import {type SocialLinksSignupProps} from "../../Components/SocialLinksSignup"
+import {type TutorialCardProps} from "../../Components/TutorialCard"
+import {type UpcomingEventsProps} from "../../Components/UpcomingEvents"
+import PageBackground from "../shared/PageBackground"
+import PageSection from "../shared/PageSection"
+import PageSections from "../shared/PageSections"
+import {type HomepageStartItemProps} from "../../Components/HomepageStartItem"
 
 export type HomePageProps = SocialLinksSignupProps & {
-  concepts?: TutorialCardProps[];
-  homepageStartProjectData: HomepageStartItemProps[];
-  contentNavigationListItems: ContentNavigationListProps;
-  flips: FlipsProps;
-  upcomingEvents: UpcomingEventsProps;
-};
+  concepts?: TutorialCardProps[]
+  homepageStartProjectData: HomepageStartItemProps[]
+  contentNavigationListItems: ContentNavigationListProps
+  flips: FlipsProps
+  upcomingEvents: UpcomingEventsProps
+}
 
 const Description = (): JSX.Element => (
   <>
-    Start your Flow journey now with the{' '}
-    <a href="https://play.flow.com/">Flow Playground</a>, or explore our
+    Start your Flow journey now with the{" "}
+    <a href='https://play.flow.com/'>Flow Playground</a>, or explore our
     comprehensive guides, resources, and references to kickstart your Flow
     development.
   </>
-);
+)
 
 const HomePage = ({
   homepageStartProjectData,
@@ -44,36 +44,33 @@ const HomePage = ({
   upcomingEvents,
 }: HomePageProps): JSX.Element => {
   return (
-    <PageBackground gradient="home">
+    <PageBackground gradient='home'>
       <LandingHeaderHome
         description={<Description />}
         discordUrl={discordUrl}
         githubUrl={githubUrl}
-        tag="onflow"
-        title="Developer Documentation"
+        tag='onflow'
+        title='Developer Documentation'
       />
       <HomepageStartList items={homepageStartProjectData} />
       <PageSections>
-        <PageSection sectionId="flips">
-          <Flips {...flips} headerLink="flips" />
-        </PageSection>
         {upcomingEvents.events.length > 0 && (
-          <PageSection sectionId="upcoming-events">
-            <UpcomingEvents {...upcomingEvents} headerLink="upcoming-events" />
+          <PageSection sectionId='upcoming-events'>
+            <UpcomingEvents {...upcomingEvents} headerLink='upcoming-events' />
           </PageSection>
         )}
-        <PageSection sectionId="explore-more-content">
+        <PageSection sectionId='explore-more-content'>
           <ContentNavigationList
             header={contentNavigationListItems.header}
             contentNavigationItems={
               contentNavigationListItems.contentNavigationItems
             }
-            headerLink="explore-more-content"
+            headerLink='explore-more-content'
           />
         </PageSection>
       </PageSections>
     </PageBackground>
-  );
-};
+  )
+}
 
-export default HomePage;
+export default HomePage
