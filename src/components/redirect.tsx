9d29efd448f32cb4a56a35418f32808715d909f3
Alex Ni <12097569+nialexsan@users.noreply.github.com>
2024-02-07 19:10:31 -0500

    native redirect

--- src/components/redirect.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-
-export function Redirect({ to }: { to: string }): null {
-  React.useEffect(() => {
-    window.location.href = useBaseUrl(to);
-  }, []);
-  return null;
-}
