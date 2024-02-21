7cf777f44ad61875b8eb37e3f4840818fa12afd7
Tom Haile <bradford.haile@gmail.com>
2024-02-21 16:53:56 -0600

    add a button for user to click to add flow evm to their metamask

---
+++ src/ui/design-system/src/lib/Components/Metamask/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const AddEVMButton = () => {
+  const addFlowEVM = async () => {
+    try {
+      // Define your network details here
+      await window?.ethereum.request({
+        method: 'wallet_addEthereumChain',
+        params: [
+          {
+            chainId: '0x29a',
+            chainName: 'Flow',
+            rpcUrls: ['https://crescendo.evm.nodes.onflow.org'],
+            iconUrls: [
+              'https://assets-global.website-files.com/5f734f4dbd95382f4fdfa0ea/65b016be9b9cf0a402a67a38_ico-flow-crescendo.png',
+            ],
+            nativeCurrency: {
+              name: 'Flow',
+              symbol: 'FLOW',
+              decimals: 18,
+            },
+            blockExplorerUrls: ['https://blockscout.com/poa/xdai/'],
+          },
+        ],
+      });
+    } catch (error) {
+      console.error('Could not add Flow EVM', error);
+    }
+  };
+
+  return <button onClick={addFlowEVM}>Add Flow EVM</button>;
+};
+
+export default AddEVMButton;
