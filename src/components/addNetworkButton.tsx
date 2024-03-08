1168c409291379ecf071d7c6fd5f414cf0963d25
Tom Haile <bradford.haile@gmail.com>
2024-03-08 14:03:18 -0600

    detect if user has flow network selected and don't prompt to add network

--- src/components/addNetworkButton.tsx
+++ src/components/addNetworkButton.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Button,
   ButtonLink,
 } from '@site/src/ui/design-system/src/lib/Components/Button/index';
 
 export const AddNetworkButton = () => {
+  const targetChainIds = [747, 646, 545]; // Your target chain IDs
+  const [isNetworkAdded, setIsNetworkAdded] = useState<boolean>(false);
+  const [chainId, setChainId] = useState<string>(''); // Flow Previewnet
+
+  const getChainId = async () => {
+    if (!window?.ethereum) return;
+    const chainId = await window?.ethereum.request({ method: 'eth_chainId' });
+    setChainId(parseInt(chainId, 16)); // Convert chainId from hex to decimal
+  };
+
+  useEffect(() => {
+    getChainId();
+  }, []);
+
   const hasEthereum = window?.ethereum !== undefined;
+
+  useEffect(() => {
+    if (targetChainIds.includes(chainId)) {
+      setIsNetworkAdded(true);
+    } else {
+      setIsNetworkAdded(false);
+    }
+  }, [chainId]);
 
   const addFlowNetwork = async () => {
     try {
@@ -36,16 +58,21 @@ export const AddNetworkButton = () => {
 
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   return hasEthereum ? (
-    <Button className='my-5 ' variant='secondary' onClick={addFlowNetwork}>
-      Add Flow Network
+    <Button
+      className="my-5 "
+      disabled={isNetworkAdded}
+      variant="secondary"
+      onClick={addFlowNetwork}
+    >
+      {isNetworkAdded ? 'Flow Network Added!' : 'Add Flow Network'}
     </Button>
   ) : (
     <ButtonLink
-      className='my-5'
-      variant='primary'
-      href='https://metamask.io/download/'
+      className="my-5"
+      variant="primary"
+      href="https://metamask.io/download/"
     >
       Install MetaMask
     </ButtonLink>
-  )
+  );
 };
