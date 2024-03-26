ece888ddd40aaa9bd5b05562e875e0a29ea45ec8
Tom Haile <bradford.haile@gmail.com>
2024-03-26 16:50:37 -0500

    add hover effect to image links

--- src/ui/design-system/src/lib/Components/HomepageStartItem/index.tsx
+++ src/ui/design-system/src/lib/Components/HomepageStartItem/index.tsx
@@ -16,7 +16,7 @@ export function HomepageStartItem({
 }: HomepageStartItemProps): React.ReactElement {
   return (
     <AppLink
-      className="flex w-full h-full cursor-pointer gap-6 rounded-lg text-white hover:text-white justify-center items-center hover:border-black"
+      className="flex w-full h-full cursor-pointer gap-6 rounded-lg text-white hover:text-white justify-center items-center hover:border-black transition-opacity duration-200 ease-out hover:opacity-90"
       to={link}
     >
       <HomepageStartItemIcons icon={icon} />
