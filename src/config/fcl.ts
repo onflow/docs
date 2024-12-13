d5f0300ef3f933339a7b058b73ae5e82053b9707
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2024-12-13 14:39:13 -0800

    Fix AN logic

--- src/config/fcl.ts
+++ src/config/fcl.ts
@@ -8,7 +8,7 @@ console.log('Dapp running on network:', flowNetwork);
 export function configureFCL(): void {
   fcl.config({
     'flow.network': flowNetwork,
-    'accessNode.api': flowNetwork ?
+    'accessNode.api': flowNetwork === 'testnet' ?
       'https://rest-testnet.onflow.org' :
       'https://rest-mainnet.onflow.org',
     'discovery.wallet': `https://fcl-discovery.onflow.org/${flowNetwork}/authn`,
