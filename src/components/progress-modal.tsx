d2fa55d7b7146766546eae27bb5a55d17d9c6534
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2024-12-19 15:21:18 -0800

    Add checklist

--- src/components/progress-modal.tsx
+++ src/components/progress-modal.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Modal from '@site/src/ui/design-system/src/lib/Components/Modal';
+import Checklist from '@site/src/components/progress-checklist';
 
 interface ProgressModalProps {
   isOpen: boolean;
@@ -7,13 +8,20 @@ interface ProgressModalProps {
 }
 
 const ProgressModal: React.FC<ProgressModalProps> = ({ isOpen, onClose }) => {
+  const profileItems = [
+    { label: "Create handle", completed: true },
+    { label: "Add Github Profile", completed: true },
+    { label: "Add how you found Flow", completed: false },
+    { label: "Add contract addresses", completed: true },
+  ];
+
   return (
     <Modal
       isOpen={isOpen}
       onClose={onClose}
       title="Progress"
     >
-      Content Here
+      <Checklist title="Profile" items={profileItems} />
     </Modal>
   );
 };
