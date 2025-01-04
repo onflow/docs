51d029ccdf171842eb69cc114684840157241e8e
Jordan Ribbink <jribbink@telus.net>
2025-01-03 17:53:07 -0800

    fixup

--- src/types/gold-star.ts
+++ src/types/gold-star.ts
@@ -1,4 +1,4 @@
-export interface ProfileSettings {
+export interface ProfileUpdate {
   handle?: string;
   referralSource?: string;
   socials?: Map<string, string>;
@@ -13,11 +13,22 @@ export interface DeployedContract {
 export interface Profile {
   handle: string;
   socials: string[];
-  completedChallenges: string[];
+  completedChallenges: Challenge[];
 }
 
 export interface ProfileResponse {
   handle: string;
   socials: string[];
-  completedChallenges: string[];
+  completedChallenges: ChallengeResponse[];
+}
+
+export interface ChallengeResponse {
+  id: string;
+  name: string;
+  description: string;
+}
+
+export interface Challenge {
+  name: string;
+  description: string;
 }
--- src/utils/gold-star.ts
+++ src/utils/gold-star.ts
@@ -1,35 +1,88 @@
-import * as fcl from '@onflow/fcl';
-
 import GetChallenges from '../../gold-star/cadence/scripts/GetChallenges.cdc';
 import GetProfile from '../../gold-star/cadence/scripts/GetProfile.cdc';
 import CreateProfile from '../../gold-star/cadence/transactions/GoldStar/CreateProfile.cdc';
 import UpdateProfile from '../../gold-star/cadence/transactions/GoldStar/UpdateProfile.cdc';
-import { ProfileResponse, ProfileSettings } from '../types/gold-star';
 
-export const getProfile = async (address: string) => {
-  const resp = fcl.query({
+import * as fcl from '@onflow/fcl';
+import {
+  ChallengeResponse,
+  Profile,
+  ProfileResponse,
+  ProfileUpdate,
+} from '../types/gold-star';
+
+/**
+ * Get the list of challenges
+ * @returns The list of challenges
+ */
+export const getChallenges = async () => {
+  const resp = (await fcl.query({
+    cadence: GetChallenges,
+  })) as ChallengeResponse[];
+
+  return resp.map((c) => ({
+    name: c.name,
+    description: c.description,
+  }));
+};
+
+/**
+ * Get the profile for the given address
+ * @param address
+ * @returns The profile for the given address
+ */
+export const getProfile = async (address: string): Promise<Profile> => {
+  const resp = (await fcl.query({
     cadence: GetProfile,
     args: (arg, t) => [arg(address, t.Address)],
-  }) as Promise<ProfileResponse>;
+  })) as ProfileResponse;
 
   return {
-    ...resp,
-    socials: resp.socials.map((social) => social.value),
+    handle: resp.handle,
+    socials: resp.socials,
+    completedChallenges: resp.completedChallenges.map((c) => ({
+      name: c.name,
+      description: c.description,
+    })),
   };
 };
 
-// Create a new profile for the user with the given settings
-export const createProfile = async (settings: ProfileSettings) => {
-  fcl.mutate({
+/**
+ * Create a new profile for the user
+ * @param profile
+ * @returns The transaction ID
+ */
+export const createProfile = async (profile: ProfileUpdate) => {
+  return await fcl.mutate({
     cadence: CreateProfile,
-    args: (arg, t) => [arg(name, t.String), arg(socials, t.Array(t.String))],
+    args: (arg, t) => [
+      arg(profile.handle, t.String),
+      arg(profile.referralSource, t.Optional(t.String)),
+      arg(profile.socials, t.Array(t.String)),
+      arg(
+        profile.deployedContracts,
+        t.Dictionary(t.Address, t.Dictionary(t.Address, t.Array(t.String))),
+      ),
+    ],
   });
 };
 
-// Fully replace the user's profile settings with new data
-export const updateProfile = async (settings: ProfileSettings) => {
-  fcl.mutate({
+/**
+ * Update the profile for the user with the given settings.  This will overwrite the existing profile.
+ * @param profile The new profile settings
+ * @returns The transaction ID
+ */
+export const setProfile = async (profile: ProfileUpdate) => {
+  return await fcl.mutate({
     cadence: UpdateProfile,
-    args: (arg, t) => [arg(name, t.String)],
+    args: (arg, t) => [
+      arg(profile.handle, t.String),
+      arg(profile.referralSource, t.Optional(t.String)),
+      arg(profile.socials, t.Array(t.String)),
+      arg(
+        profile.deployedContracts,
+        t.Dictionary(t.Address, t.Dictionary(t.Address, t.Array(t.String))),
+      ),
+    ],
   });
 };
