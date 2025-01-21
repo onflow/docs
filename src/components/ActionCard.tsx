de9aa188d0fa977e324f8d69e7cf6b203ad6288f
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2025-01-21 15:40:27 -0800

    Fix icon color and width

--- src/components/ActionCard.tsx
+++ src/components/ActionCard.tsx
@@ -24,9 +24,9 @@ const ActionCard: React.FC<ActionCardProps> = ({
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          strokeWidth={1.5}
+          strokeWidth={2}
           stroke="currentColor"
-          className="w-6 h-6 text-white"
+          className="w-6 h-6 black"
         >
           <path
             strokeLinecap="round"
