9cbb07701b9f5647c975224f76d6e695a35a7c14
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2025-01-21 15:54:55 -0800

    Add overlay variant

--- src/components/ActionCard.tsx
+++ src/components/ActionCard.tsx
@@ -7,6 +7,7 @@ interface ActionCardProps {
   heading: string;
   description: string;
   onClick?: () => void;
+  variant?: 'default' | 'overlay';
 }
 
 const ActionCard: React.FC<ActionCardProps> = ({
@@ -15,35 +16,70 @@ const ActionCard: React.FC<ActionCardProps> = ({
     heading,
     description,
     onClick,
+    variant = 'default',
   }) => {
   const cardBg = colors[cardColor].dark;
   const iconBg = colors[iconColor].light;
 
   return (
-    <div onClick={onClick} className={`p-6 rounded-lg shadow-lg hover:scale-105 transition-transform cursor-pointer ${cardBg}`}>
-      <div className={`w-10 h-10 rounded-md flex items-center justify-center mb-4 ${iconBg}`}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={2}
-          stroke="currentColor"
-          className="w-6 h-6 black"
+    <div
+      onClick={onClick}
+      className={`relative flex flex-col justify-between p-6 rounded-lg shadow-lg hover:scale-105 transition-transform cursor-pointer ${cardBg}`}
+    >
+      {variant === 'overlay' && (
+        <div
+          className={`absolute -top-5 left-6 w-10 h-10 rounded-md flex items-center justify-center ${iconBg}`}
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-          />
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
-          />
-        </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={2}
+            stroke="currentColor"
+            className="w-6 h-6 text-black"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+            />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+            />
+          </svg>
+        </div>
+      )}
+      {variant === 'default' && (
+        <div
+          className={`w-10 h-10 rounded-md flex items-center justify-center mb-8 ${iconBg}`}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={2}
+            stroke="currentColor"
+            className="w-6 h-6 text-black"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+            />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+            />
+          </svg>
+        </div>
+      )}
+      <div className="mt-auto">
+        <h3 className="text-xl font-semibold text-white mb-2">{heading}</h3>
+        <p className="text-sm text-gray-100">{description}</p>
       </div>
-      <h3 className="text-xl font-semibold text-white mb-2 mt-12">{heading}</h3>
-      <p className="text-sm text-gray-100">{description}</p>
     </div>
   );
 };
