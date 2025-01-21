6e99c967ce3f803ee042cd9fce658f2e856edb67
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2025-01-21 15:56:05 -0800

    Reuse icon

--- src/components/ActionCard.tsx
+++ src/components/ActionCard.tsx
@@ -10,6 +10,28 @@ interface ActionCardProps {
   variant?: 'default' | 'overlay';
 }
 
+const LocationIcon: React.FC = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={2}
+    stroke="currentColor"
+    className="w-6 h-6 text-black"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+    />
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+    />
+  </svg>
+);
+
 const ActionCard: React.FC<ActionCardProps> = ({
     iconColor = 'white',
     cardColor = 'black',
@@ -30,50 +52,14 @@ const ActionCard: React.FC<ActionCardProps> = ({
         <div
           className={`absolute -top-5 left-6 w-10 h-10 rounded-md flex items-center justify-center ${iconBg}`}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={2}
-            stroke="currentColor"
-            className="w-6 h-6 text-black"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-            />
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
-            />
-          </svg>
+          <LocationIcon />
         </div>
       )}
       {variant === 'default' && (
         <div
-          className={`w-10 h-10 rounded-md flex items-center justify-center mb-8 ${iconBg}`}
+          className={`w-10 h-10 rounded-md flex items-center justify-center mb-6 ${iconBg}`}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={2}
-            stroke="currentColor"
-            className="w-6 h-6 text-black"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-            />
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
-            />
-          </svg>
+          <LocationIcon />
         </div>
       )}
       <div className="mt-auto">
