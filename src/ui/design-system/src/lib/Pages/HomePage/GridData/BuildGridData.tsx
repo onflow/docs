959c598005f07da2ab25b838afed40b9d987cf1b
Brian Doyle <briandoyle81@gmail.com>
2025-04-01 11:38:11 -0400

    Move cursor doc and minor cleanup

--- src/ui/design-system/src/lib/Pages/HomePage/GridData/BuildGridData.tsx
+++ src/ui/design-system/src/lib/Pages/HomePage/GridData/BuildGridData.tsx
@@ -36,7 +36,7 @@ export const buildGridData = {
           cardColor: 'black',
           icon: 'hello-world',
           variant: 'horizontal',
-          href: 'evm/about',
+          href: 'tools/cursor',
         },
       ],
     },
