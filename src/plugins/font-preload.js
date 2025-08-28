7090f9d19b801dade1c151e3a21402fc451641a3
Brian Doyle <briandoyle81@gmail.com>
2025-08-28 11:36:19 -0400

    Convert fonts to woff2

--- src/plugins/font-preload.js
+++ src/plugins/font-preload.js
@@ -4,14 +4,14 @@ module.exports = function fontPreloadPlugin() {
     injectHtmlTags() {
       return {
         headTags: [
-          // Preload critical fonts
+          // Preload critical fonts (WOFF2 format - 66% smaller!)
           {
             tagName: 'link',
             attributes: {
               rel: 'preload',
-              href: '/fonts/inter/Inter-Regular.ttf',
+              href: '/fonts/inter-woff2/Inter-Regular.woff2',
               as: 'font',
-              type: 'font/ttf',
+              type: 'font/woff2',
               crossorigin: 'anonymous',
             },
           },
@@ -19,9 +19,9 @@ module.exports = function fontPreloadPlugin() {
             tagName: 'link',
             attributes: {
               rel: 'preload',
-              href: '/fonts/inter/Inter-Medium.ttf',
+              href: '/fonts/inter-woff2/Inter-Medium.woff2',
               as: 'font',
-              type: 'font/ttf',
+              type: 'font/woff2',
               crossorigin: 'anonymous',
             },
           },
@@ -29,9 +29,9 @@ module.exports = function fontPreloadPlugin() {
             tagName: 'link',
             attributes: {
               rel: 'preload',
-              href: '/fonts/inter/Inter-SemiBold.ttf',
+              href: '/fonts/inter-woff2/Inter-SemiBold.woff2',
               as: 'font',
-              type: 'font/ttf',
+              type: 'font/woff2',
               crossorigin: 'anonymous',
             },
           },
@@ -39,38 +39,46 @@ module.exports = function fontPreloadPlugin() {
             tagName: 'link',
             attributes: {
               rel: 'preload',
-              href: '/fonts/inter/Inter-Bold.ttf',
+              href: '/fonts/inter-woff2/Inter-Bold.woff2',
               as: 'font',
-              type: 'font/ttf',
+              type: 'font/woff2',
               crossorigin: 'anonymous',
             },
           },
-          // Add @font-face declarations for the preloaded fonts
+          // Add @font-face declarations for the preloaded fonts (WOFF2 with TTF fallbacks)
           {
             tagName: 'style',
             attributes: {},
             innerHTML: `
               @font-face {
                 font-family: "Inter";
-                src: local("Inter"), url("/fonts/inter/Inter-Regular.ttf") format("truetype");
+                src: local("Inter"), 
+                     url("/fonts/inter-woff2/Inter-Regular.woff2") format("woff2"),
+                     url("/fonts/inter/Inter-Regular.ttf") format("truetype");
                 font-weight: 400;
                 font-display: swap;
               }
               @font-face {
                 font-family: "Inter";
-                src: local("Inter"), url("/fonts/inter/Inter-Medium.ttf") format("truetype");
+                src: local("Inter"), 
+                     url("/fonts/inter-woff2/Inter-Medium.woff2") format("woff2"),
+                     url("/fonts/inter/Inter-Medium.ttf") format("truetype");
                 font-weight: 500;
                 font-display: swap;
               }
               @font-face {
                 font-family: "Inter";
-                src: local("Inter"), url("/fonts/inter/Inter-SemiBold.ttf") format("truetype");
+                src: local("Inter"), 
+                     url("/fonts/inter-woff2/Inter-SemiBold.woff2") format("woff2"),
+                     url("/fonts/inter/Inter-SemiBold.ttf") format("truetype");
                 font-weight: 600;
                 font-display: swap;
               }
               @font-face {
                 font-family: "Inter";
-                src: local("Inter"), url("/fonts/inter/Inter-Bold.ttf") format("truetype");
+                src: local("Inter"), 
+                     url("/fonts/inter-woff2/Inter-Bold.woff2") format("woff2"),
+                     url("/fonts/inter/Inter-Bold.ttf") format("truetype");
                 font-weight: 700;
                 font-display: swap;
               }
