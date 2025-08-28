3cbd87085bec3434d12b2add136fc1a1631cc3a5
Brian Doyle <briandoyle81@gmail.com>
2025-08-28 10:45:41 -0400

    Remove extra inter copies

--- src/plugins/font-preload.js
+++ src/plugins/font-preload.js
@@ -4,6 +4,7 @@ module.exports = function fontPreloadPlugin() {
     injectHtmlTags() {
       return {
         headTags: [
+          // Preload critical fonts
           {
             tagName: 'link',
             attributes: {
@@ -43,6 +44,37 @@ module.exports = function fontPreloadPlugin() {
               type: 'font/ttf',
               crossorigin: 'anonymous',
             },
+          },
+          // Add @font-face declarations for the preloaded fonts
+          {
+            tagName: 'style',
+            attributes: {},
+            innerHTML: `
+              @font-face {
+                font-family: "Inter";
+                src: local("Inter"), url("/fonts/inter/Inter-Regular.ttf") format("truetype");
+                font-weight: 400;
+                font-display: swap;
+              }
+              @font-face {
+                font-family: "Inter";
+                src: local("Inter"), url("/fonts/inter/Inter-Medium.ttf") format("truetype");
+                font-weight: 500;
+                font-display: swap;
+              }
+              @font-face {
+                font-family: "Inter";
+                src: local("Inter"), url("/fonts/inter/Inter-SemiBold.ttf") format("truetype");
+                font-weight: 600;
+                font-display: swap;
+              }
+              @font-face {
+                font-family: "Inter";
+                src: local("Inter"), url("/fonts/inter/Inter-Bold.ttf") format("truetype");
+                font-weight: 700;
+                font-display: swap;
+              }
+            `,
           },
         ],
       };
