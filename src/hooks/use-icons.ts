161631aa822a231b88cfd6fdc5e043540e7ac5fd
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2026-01-01 19:04:10 -0800

    Remove

--- src/hooks/use-icons.ts
+++ src/hooks/use-icons.ts
@@ -9,11 +9,12 @@ const iconMap: Record<IconName, string> = {
   [IconName.FLOW_CADENCE]: '/images/icons/flow-cadence.svg',
   [IconName.EVM_ON_FLOW]: '/images/icons/evm-on-flow.svg',
   [IconName.RANDOM]: '/images/icons/random.svg',
-  [IconName.BATCHED_EVM_TRANSACTIONS]: '/images/icons/batched-evm-transactions.svg',
+  [IconName.BATCHED_EVM_TRANSACTIONS]:
+    '/images/icons/batched-evm-transactions.svg',
   [IconName.FLOW_CLIENT_LIBRARY]: '/images/icons/flow-client-library.svg',
   [IconName.TOOLS]: '/images/icons/flow-tools.svg',
   [IconName.FAUCET]: '/images/icons/Faucet.svg',
-  
+
   // GrowGridData icons
   [IconName.GROW]: '/images/icons/flow-grow.svg',
   [IconName.BUILDER_CREDITS]: '/images/icons/builder-credits.svg',
@@ -22,20 +23,21 @@ const iconMap: Record<IconName, string> = {
   [IconName.STARTUP_SUPPORT]: '/images/icons/startup-support.svg',
   [IconName.VCS_AND_FUNDS]: '/images/icons/vcs-&-funds.svg',
   [IconName.ACCESS_INCREDIBLE_IP]: '/images/icons/access-incredible-ip.svg',
-  
+
   // Other commonly used icons
   [IconName.LEARN]: '/images/icons/flow-learn.svg',
   [IconName.GS_HELLO_WORLD]: '/images/icons/gs-hello-world.svg',
   [IconName.CADENCE]: '/images/icons/cadence-logo-mark-black-1.svg',
   [IconName.SOLIDITY]: '/images/icons/flow-evm.svg',
-  
+
   // Feature icons
   [IconName.FEATURE_WHY_FLOW_ICON]: '/images/icons/feature-why-flow-icon.svg',
   [IconName.FEATURE_WAND_ICON]: '/images/icons/feature-wand-icon.svg',
   [IconName.FEATURE_STACKS_ICON]: '/images/icons/feature-stacks-icon.svg',
-  [IconName.FEATURE_CODE_SCRIPTS_ICON]: '/images/icons/feature-code-scripts-icon.svg',
+  [IconName.FEATURE_CODE_SCRIPTS_ICON]:
+    '/images/icons/feature-code-scripts-icon.svg',
   [IconName.FEATURE_EVM_ICON]: '/images/icons/feature-evm-icon.svg',
-  
+
   // Other icons
   [IconName.ROADMAP]: '/images/icons/roadmap.svg',
   [IconName.CADENCE_COURSE]: '/images/icons/cadence-course.svg',
@@ -48,12 +50,12 @@ const iconMap: Record<IconName, string> = {
   [IconName.TUTORIALS]: '/images/icons/tutorials.svg',
   [IconName.CROSS_VM_BRIDGE]: '/images/icons/cross-vm-bridge.svg',
   [IconName.FORUM]: '/images/icons/forum.svg',
-  
+
   // Social media icons
   [IconName.DISCORD]: '/images/icons/discord.svg',
   [IconName.X_COM]: '/images/icons/x.com.svg',
   [IconName.GITHUB]: '/images/icons/github.svg',
-  
+
   // Ecosystem icons (these use a different path pattern)
   [IconName.QUICKNODE]: '/img/ecosystem/quicknode.svg',
   [IconName.OLYMPIX_LOGO]: '/img/ecosystem/olympix-logo.svg',
@@ -61,8 +63,6 @@ const iconMap: Record<IconName, string> = {
   [IconName.ALCHEMY]: '/img/ecosystem/alchemy.svg',
   [IconName.THIRDWEB]: '/img/ecosystem/thirdweb.svg',
   [IconName.UNIBLOCK]: '/img/ecosystem/uniblock.svg',
-  
-  // Security icons
   [IconName.SHIELD_CHECK]: '/images/icons/shield-check.svg',
 };
 
